d9d0070fb1bb00130f38600b593bbe2b07ad76ea
DANNY BERNARD <DANNY.BERNARD@hotmail.com>
2018-09-14 05:13:57 +0200

    Entiddades Lista

---
+++ CotizarArticulos/Entidades/Articulos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CotizarArticulos.Entidades
+{
+    class Articulos
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Descricion { get; set; }
+        public DateTime FechaDeVencimiento { get; set; }
+        public float Precio { get; set; }
+        public int Exitencia { get; set; }
+        public int Cantidad { get; set; }
+
+        public Articulos()
+        {
+            ID = 0;
+            Nombre = string.Empty;
+            Descricion = string.Empty;
+            FechaDeVencimiento = DateTime.Now;
+            Precio = 0;
+            Exitencia = 0;
+            Cantidad = 0;
+        }
+    }
+}
